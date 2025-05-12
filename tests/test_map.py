5b9a7bd6cd9a1f4641d3ebb0abb5b0ae56dc4a3f
anntulyholovets <anntulyholovets@knu.com>
2025-05-13 00:27:10 +0300

    Add new tests for Map

--- tests/test_map.py
+++ tests/test_map.py
@@ -10,3 +10,10 @@ def simple_map():
 def test_is_wall_within_bounds(simple_map):
     """Перевірка: клітинка не є стіною після створення"""
     assert simple_map.is_wall(1, 1) is False
+
+def test_is_wall_out_of_bounds(simple_map):
+    """Перевірка: поза межами мапи — завжди стіна"""
+    assert simple_map.is_wall(-1, 0) is True
+    assert simple_map.is_wall(3, 0) is True
+    assert simple_map.is_wall(0, -1) is True
+    assert simple_map.is_wall(0, 3) is True
