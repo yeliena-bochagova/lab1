effe816335cef1b332a6c4c2c84a269e92ed0540
anntulyholovets <anntulyholovets@knu.com>
2025-05-13 00:29:14 +0300

    Add new tests for Tile

--- tests/test_tile.py
+++ tests/test_tile.py
@@ -4,3 +4,8 @@ def test_tile_default():
     """Перевірка: за замовчуванням Tile не є стіною"""
     tile = Tile()
     assert tile.is_wall is False
+
+def test_tile_with_wall():
+    """Перевірка: Tile створюється як стіна, якщо is_wall=True"""
+    tile = Tile(is_wall=True)
+    assert tile.is_wall is True
